2d62670d7d393a159c4587eb59ed8e8c71fa54b0
dingjingmaster <dingjing@live.cn>
2023-12-22 18:45:35 +0800

    [完成]     目前可以通过GDM启动桌面环境 [已知问题]     无法使用 ibus 或 fcitx

--- data/data.cmake
+++ data/data.cmake
@@ -1,2 +1,2 @@
-install(FILES ${CMAKE_SOURCE_DIR}/data/start-graceful-linux DESTINATION /usr/bin/)
-install(FILES ${CMAKE_SOURCE_DIR}/data/graceful-linux.desktop DESTINATION /usr/share/xsessions/)
+install(PROGRAMS    ${CMAKE_SOURCE_DIR}/data/start-graceful-linux DESTINATION /usr/bin/)
+install(FILES       ${CMAKE_SOURCE_DIR}/data/graceful-linux.desktop DESTINATION /usr/share/xsessions/)
