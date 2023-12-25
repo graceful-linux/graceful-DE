12483e086824c45f0d035b42b93f74efbea4a2a9
dingjingmaster <dingjing@live.cn>
2023-12-25 11:10:09 +0800

    [添加]  双屏支持

---
+++ app/wm/patch/bar_tagicons.h
@@ -0,0 +1,9 @@
+enum
+{
+    DEFAULT_TAGS,
+    ALTERNATIVE_TAGS,
+    ALT_TAGS_DECORATION,
+};
+
+static char * tagicon(Monitor *m, int tag);
+
--- app/wm/util.h
+++ app/wm/util.h
@@ -1,7 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
+#ifndef MAX
 #define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#endif
+#ifndef MIN
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#endif
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
 #ifdef _DEBUG
@@ -12,7 +16,4 @@
 
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
-int normalizepath(const char *path, char **normal);
-int mkdirp(const char *path);
-int parentdir(const char *path, char **parent);
-int nullterminate(char **str, size_t *len);
+
